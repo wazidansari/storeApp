9fde07b1099814483d45a629ff6fd8a2571190c9
Wazid H. Ansari <wansar03@in.ibm.com>
2021-04-03 20:58:14 +0530

    Basic setup of dropwizard and gradle

---
+++ src/main/java/com/project/MyStoreApplication.java
@@ -0,0 +1,7 @@
+package com.project;
+
+public class MyStoreApplication {
+    public static void main(String args[]){
+        System.out.println("Hello World!!");
+    }
+}
---
+++ src/test/java/com/store/MyStoreApplicationTest.java
@@ -0,0 +1,11 @@
+package com.store;
+
+import com.project.MyStoreApplication;
+import org.junit.jupiter.api.Test;
+
+public class MyStoreApplicationTest {
+    @Test
+    public void testSomething(){
+        MyStoreApplication.main(new String[]{});
+    }
+}
